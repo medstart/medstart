e5223b36a2bbae411c195da86aad73068d17967e
apnaadda <finallystart15@gmail.com>
2015-07-23 12:39:27 +0530

    all setup done

---
+++ pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "pages/index1.html")
+
+def register(request):
+    return render(request, "register.html")
---
+++ start/views.py
@@ -0,0 +1,7 @@
+# __author__ = 'komalkgupta'
+#
+# from django.shortcuts import render_to_response, render
+#
+#
+# def index(request):
+#     return render(request, 'index.html')
